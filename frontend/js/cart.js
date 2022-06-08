1339fdb909c2dac72da2cbe78f48577777229fca
master <master.code.develop@gmail.com>
2022-06-08 15:25:52 +0200

    frontend cart

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,262 +1,242 @@
-var localStorageProducts = JSON.parse(localStorage.getItem("products"));
-products = [];
+var products = [],
+localStorageProducts = JSON.parse(localStorage.getItem("products"));
+//console.log(localStorageProducts)
 const items = document.getElementById("cart__items");
+const section = document.getElementById("cart__items");
 
-// S'il y a bien quelques chose dans le localStorage :
-if (localStorageProducts && localStorageProducts.length != 0) {
+/*
+ * Envoie une requet ver le serveur pour récupérer les informations sur les produits .
+ */
+if ( localStorageProducts && localStorageProducts.length != 0 ) {
+    fetch('http://localhost:3000/api/products')
+        .then(response  => response.json()
+        .then(data => {
+            // console.log(data);
+            products = data;
 
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
-    for (let i = 0; i < localStorageProducts.length; i++) {
+            const productsListOrdered = localStorageProducts.sort((a, b) => {
+                if (a.nameItem < b.nameItem) {return -1;}
+                if (a.nameItem > b.nameItem) {return 1};
+                return 0;
+            });
+            for (const item of productsListOrdered) {
+                const {idItem, colorItem, qtity} = item;
+                createItem({idItem, colorItem, qtity})
+            }
+            updateQuantityAndPrice();
+            
+        }))
+        .catch(err => console.log(err));
+} else {
+    // creation de message
+    const message = `
+        <p style="text-align: center; margin-bottom: 50px;" >
+            Votre panier est vide pour le moment
+        </p>
+    `
+    //imbrication de message dans la section cart__items
+    items.innerHTML += message;
 
-        // recupérer les information de l'API de chaque produit
-        addProduct();
-        const {idItem, colorItem} =localStorageProducts[i],
-        index = localStorageProducts.findIndex( item => item.idItem+item.colorItem == idItem+colorItem ),
-        product = products[index];
+    //ne pas afficher les blocs .cart__price et .cart__order
+    const cartPrice = document.querySelector('.cart__price'),
+    cartOrder = document.querySelector('.cart__order');
 
-        // creation et imbrication de l'article
-        const articleChilds = `
+    cartPrice.style.display='none';
+    cartOrder.style.display='none';
+}
+
+
+/**
+ * Créer un nouvelle article dans la section #cart__items
+ * 
+ * @param { String } idItem 
+ * @param { String } colorItem
+ * @param { String } qtity
+ * 
+ */
+function createItem({idItem, colorItem, qtity}) {
+    index = products.findIndex( item => item._id  == idItem );
+    const { name, price, imageUrl, description, altTxt } = products[index];
+
+    // créer l'article
+    const article = document.createElement("article");
+    article.innerHTML = `
+        <article class="cart__item" data-id="${idItem}" data-color="${colorItem}">
             <div class="cart__item__img">
-                <img src="${product.imageUrlItem}" alt="Photographie de ${product.nameItem}">
+                <img src="${imageUrl}" alt="${altTxt}">
             </div>
             <div class="cart__item__content">
                 <div class="cart__item__content__description">
-                    <h2>${product.nameItem}</h2>
-                    <p>${product.colorItem}</p>
-                    <p>${product.priceItem} €</p>
+                    <h2>${name}</h2>
+                    <p>${colorItem}</p>
+                    <p>${price} €</p>
                 </div>
                 <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                         <p>Qté :</p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.qtity}">
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${qtity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                         <p class="deleteItem">Supprimer</p>
                     </div>
                 </div>
             </div>
-        `
-        const article = document.createElement("article");
-        article.className = "cart__item";
-        article.dataset.id = product.idItem;
-        article.dataset.color = product.colorItem;
-        article.innerHTML = articleChilds;
-        items.appendChild(article);
-
-        total();
-    }
-}
-// Si le localStorage est vide :
-else {
-    // creation de message
-    const 
-        message = document.createElement("p"),
-        cartPrice = document.querySelector('.cart__price'),
-        cartOrder = document.querySelector('.cart__order');
-
-    message.innerText = "Votre panier est vide pour le moment";
-    message.style = `
-        text-align: center;
-        margin-bottom: 50px;
+        </article>
     `;
-    //imbrication de message dans la section cart__items
-    items.appendChild(message);
 
-    //ne pas afficher les blocs .cart__price et .cart__order
-    cartOrder.style.display='none';
+    // si la quantité de l'article change 
+    const itemQuantity = article.querySelector('.itemQuantity');
+    itemQuantity.addEventListener('change', onChangeQtity  );
+    
+    // si on clique sur supprimer
+    const deleteItem = article.querySelector('.deleteItem');
+    deleteItem.addEventListener('click', removeItem);
+
+    // Imbrication de l'article dans la section
+    items.appendChild(article);
 }
 
-const inputsQtity = document.querySelectorAll(".itemQuantity");
-inputsQtity.forEach((inputQtity) => {
-    inputQtity.addEventListener('change', (e) => {
-        const 
-            newQtity = e.target.value,
-            {id, color} = e.target.closest("article").dataset,
-            idSpec = id+color,
-            productIndex = localStorageProducts.findIndex( item => item.idSpec == idSpec );
-        
-        // si la quantité de produit change et dans les normes alors ça met à jour les changement
-        if(newQtity != localStorageProducts[productIndex].qtity && (0 < newQtity && newQtity <= 100 )){
-            localStorageProducts[productIndex].qtity = newQtity
-            localStorage.setItem("products", JSON.stringify(localStorageProducts));
-            total()
-        }
-        // si non on supprime l'article
-        else {
-            deleteElement(productIndex)
-        }
-    })
-})
-function addProduct() {
-    console.log('add')
-}
-function total() {
-    var totalQuantity = 0
-    var totalPrice = 0
+/**
+ * Mets à jour la quantité ainsi que le prix total
+ */
+function updateQuantityAndPrice() { 
+    let 
+        totalQuantity = 0,
+        totalPrice = 0;
 
-    for(const product of localStorageProducts){
-        totalQuantity += Number(product.qtity)
-        totalPrice += Number(product.qtity) * Number(product.priceItem)
+    if (localStorageProducts && products) {
+        for (const item of localStorageProducts) {
+            const 
+                {idItem, colorItem, qtity} = item,
+                index = products.findIndex( item => item._id  == idItem ),
+                price = products[index].price;
+
+            totalPrice += Number(qtity) * Number(price);
+            totalQuantity += Number(qtity);
+        }
     }
 
     document.getElementById("totalQuantity").innerHTML = totalQuantity
     document.getElementById("totalPrice").innerHTML = totalPrice
 }
 
-// deleteItem || Supprimer l'article en cliquant sur supprimer
-const item = document.querySelectorAll(".deleteItem")
-item.forEach((item) => {
-    item.addEventListener('click', e => {
-         
-        const 
-            {id, color} = e.target.closest("article").dataset,
-            idSpec = id+color,
-            productIndex = localStorageProducts.findIndex( item => item.idSpec == idSpec ),
-            product = localStorageProducts[productIndex];
-        
-        // si confirme alors ça supprime l'article
-        if(confirm(`Vous êtes sur le point de suprimer cette article : ${product.nameItem+"  "+product.colorItem}`)){
-            deleteElement(productIndex);
-        }
-    })
-})
+    // On écoute le changement de chaque élément input
+    onChangeQtity = (e) => {
+        const newQtity = e.target.value,
+        {id, color} = e.target.closest("article").dataset,
+        idSpec = id+color,
+        productIndex = localStorageProducts.findIndex( item => item.idItem + item.colorItem == idSpec );
 
-function deleteElement(index) {
-    localStorageProducts.splice(index, 1)
+        // si la quantité de produit change et dans les normes alors ça met à jour les changement
+        if(newQtity != localStorageProducts[productIndex].qtity && (0 < newQtity && newQtity <= 100 )){
+            localStorageProducts[productIndex].qtity = newQtity
+            localStorage.setItem("products", JSON.stringify(localStorageProducts));
+            updateQuantityAndPrice()
+        }
+        // si non on supprime l'article
+        else {
+            removeItem(e)
+        }
+    }
+
+function removeItem (e){
+    const {id, color} = e.target.closest("article").dataset,
+    productIndex = localStorageProducts.findIndex( item => item.idItem + item.colorItem == id+color );
+
+    localStorageProducts.splice(productIndex, 1)
     localStorage.setItem("products", JSON.stringify(localStorageProducts))
     window.location.reload()
 }
 
 
 
-
-firstName = document.getElementById("firstName"),
-lastName = document.getElementById("lastName"),
-address = document.getElementById("address"),
-city = document.getElementById("city"),
-email = document.getElementById("email")
-formValid = true;
-
-errorfirstName = false
-
-function crctr(field, value) {
-    if(field == "address"){
-        return /(?!^\d+$)^[^@&"()!_$*€£`+=\/;?#]+$/.test(value)
-    }else if (field == "email"){
-        return /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(value)
-    }else{
-        return /^[^@&"()!_$*€£`+=\/;?#\d]+$/.test(value)
-    }
-}
-
-function isValid(field, value, errorMsg, msgVoid, msg){
-    if(value == ""){
-        errorMsg.innerText = msgVoid
-    }else if( ! crctr(field, value) ){
-        errorMsg.innerText = msg
-    }else{
-        errorMsg.innerText = ""
-    }
-}
-
-// verification prénom
-firstName.addEventListener("change", (e) => {
-    const field = 'name',
-    errorMsg = document.getElementById("firstNameErrorMsg"),
-    message = "Veuillez saisir votre prénom !";
-    cPName = verifNames({e, name, errorMsg, message});
-})
-
-// verification nom
-lastName.addEventListener("change", (e) => {
-    const field = 'name',
-    errorMsg = document.getElementById("firstNameErrorMsg"),
-    message = "Veuillez saisir votre nom !";
-    cName = verifNames({e, name, errorMsg, message});
-})
-
-
-
-function verifNames({e, field, errorMsg, message}) {
-    const test = () => {
-        if(field == 'name') {
-            /^[^@&"()!_$*€£`+=\/;?#\d]+$/.test(e.target.value)
-        }
-    };
-    if (test) {
-        e.target.style.border = "0";
-        errorMsg.innerText = "";
-        return 0;
-    } else {
-        e.target.style.border = "2px solid red"
-        errorMsg.innerText = message;
-        return 1;
-    }
-}
-
-
-// verification adresse
-address.addEventListener("change", () => {
-    const value = address.value
-    const errorMsg = document.getElementById("addressErrorMsg")
-    isValid("address", value, errorMsg, "Veuillez saisir votre addresse !", "Veuillez saisir une addresse correct !")
-})
-
-// verification city
-city.addEventListener("change", () => {
-    const value = city.value
-    const errorMsg = document.getElementById("cityErrorMsg")
-    isValid("city", value, errorMsg, "Veuillez saisir votre ville !", "Veuillez saisir une ville correct !")
-})
-
-// verification email
-email.addEventListener("change", () => {
-    const value = email.value
-    const errorMsg = document.getElementById("emailErrorMsg")
-    isValid("email", value, errorMsg, "Veuillez saisir votre email !", "Veuillez saisir un email correct !")
-})
-
-
 // order
-var formOrder = {}
 const 
     orderButton = document.getElementById("order"),
     form = document.querySelector('.cart__order__form');
 
-orderButton.addEventListener('click', ()=>{
+for (let i = 0; i < form.children.length-1; i++) {
+    const input = form.children[i];
+    input.onchange = (e) => {
+        validate(e);
+    }
+    
+}
+const errorMessage = (name) => {
+    if (name == 'firstName') {
+        return "Veuillez saisir votre prénom !";
+    } else if (name == 'lastName') {
+        return "Veuillez saisir votre nom !";
+    } else if (name == 'address') {
+        return "Veuillez saisir votre adresse postal !";
+    } else if (name == 'city') {
+        return "Veuillez saisir le nom de votre ville !";
+    } else if (name == 'email') {
+        return "Veuillez saisir votre adresse email !";
+    } else {
+        return "error"
+    }
+}
 
-    if(firstName.value == "" || lastName.value == "" || address.value == "" || city.value == "" || email.value == ""){
-        for (let i = 0; i < form.length-1; i++) {
-            const e = form[i];
-            if(e.value == '') {
-                e.style.border = "2px solid red";
-            }
+const regex = (name) => {
+    if(name == 'firstName' || name =='lastName' || name == 'city' ) {
+        return /^[^@&"()!_$*€£`+=\/;?#\d]+$/;
+    } else if(name == 'address') {
+        return /(?!^\d+$)^[^@&"()!_$*€£`+=\/;?#]+$/;
+    } else if (name == 'email') {
+        return /^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/i;
+    }
+}
+function validate(e) {
+    const { name, value, style } = e.target,
+    errorMsg = document.getElementById(name+"ErrorMsg");
+
+    if (regex(name).test(value) == true ) {
+        style.border = "0";
+        errorMsg.innerText = "";
+        return 0;
+    } else {
+        style.border = "2px solid red"
+        errorMsg.innerText = errorMessage(name);
+        return 1;
+    }
+}
+orderButton.addEventListener('click', ()=>{
+    let error = 0;
+    for (let i = 0; i < form.length-1; i++) {
+        const e = form[i],
+        errorMsg = document.getElementById(e.name+"ErrorMsg");
+        if(e.value == '') {
+            e.style.border = "2px solid red";
+            errorMsg.innerText = errorMessage(e.name);
+            error += 1;
         }
+    }
         //alert("Veuillez remplir tous les champs")
-    }else{
-        formOrder = {
+    if (error == 0) {
+        const formOrder = {
             firstName: firstName.value,
             lastName: lastName.value,
             address: address.value,
             city: city.value,
             email: email.value
         }
-        requestOrder()
+        requestOrder(formOrder)
     }
     
 })
-const localStorageProductsID = []
-for (const item of localStorageProducts) {
-    localStorageProductsID.push(item.idItem)
-}
-function requestOrder() {
+
+
+function requestOrder(formOrder) {
     // Données à envoyer
+    const localStorageProductsID = []
+    for (const item of localStorageProducts) {
+        localStorageProductsID.push(item.idItem)
+    }
+
     const order = {
         contact: formOrder,
-        localStorageProducts: localStorageProductsID
+        products: localStorageProductsID
     };
     console.log(order)
     // En-tête de la requête
@@ -266,7 +246,7 @@ function requestOrder() {
         headers: {"Content-Type": "application/json"},
     };
 
-    fetch("http://localhost:3000/api/localStorageProducts/order", entete)
+    fetch("http://localhost:3000/api/products/order", entete)
         .then((response) => {
             // On vérifie qu'on reçoit bien un status HTTP 201 c'est à dire que la requête a réussi et qu'une ressource a été créée en conséquence
             if (response.status == 201) {
@@ -278,10 +258,12 @@ function requestOrder() {
             }
         })
         .then((data) => {
-            response(data.orderId);
+            localStorage.clear();
+            document.location.href = `confirmation.html?id=${data.orderId}`;
         })
-}
-function response(orderId) {
-    localStorage.clear();
-    document.location.href = `confirmation.html?id=${orderId}`;
+        .catch(
+            (error) => {
+              console.log(error);
+            }
+        );
 }
